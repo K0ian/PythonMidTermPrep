1f33e8c292da92ce05543b30dff1d372087bbf23
Kian <kianluis1382@gmail.com>
2025-02-19 17:04:22 +0100

    Question 6

--- midterm exam.py
+++ midterm exam.py
@@ -1,16 +1,9 @@
-import datetime
+numbers = [1, 2, 3]
+numbers[0] = 10  # Modifying the first element
+print(numbers)  # Output: [10, 2, 3]
 
-a = 7
-b = 2
-today = datetime.datetime.today()
-day_of_week = today.weekday()
-month_of_year = today.month
-a = a + day_of_week
-b += month_of_year
 
-print(a)
-print(b)
-c = a + b
-print(c)
-d = "xyz" * (c // 3)
-print(d)
+text = "hello"
+text = "H" + text[1:]  # Creating a new string
+print(text)  # Output: Hello
+
